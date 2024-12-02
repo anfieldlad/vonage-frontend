34f6cde22910ae324f4a6b8e864666bda77a93ee
bobalonso99 <bobalonso99@gmail.com>
2024-12-02 04:20:45 +0000

    update UI waiting room

--- src/components/WaitingRoom.js
+++ src/components/WaitingRoom.js
@@ -5,12 +5,12 @@ import './WaitingRoom.css'; // Import CSS file
 const WaitingRoom = () => {
   const [roomName, setRoomName] = useState('');
   const [userName, setUserName] = useState('');
-  const [role, setRole] = useState('publisher'); // Default role
   const navigate = useNavigate();
 
   const joinRoom = async () => {
     try {
       const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+      const role = 'publisher'; // Role otomatis diatur sebagai 'publisher'
 
       // Fetch apiKey, sessionId, and token from backend
       const response = await fetch(`${BACKEND_URL}/api/join-room`, {
@@ -52,38 +52,6 @@ const WaitingRoom = () => {
           onChange={(e) => setUserName(e.target.value)}
           required
         />
-        <div className="radio-group">
-          <label>
-            <input
-              type="radio"
-              name="role"
-              value="publisher"
-              checked={role === 'publisher'}
-              onChange={(e) => setRole(e.target.value)}
-            />
-            Publisher
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="role"
-              value="subscriber"
-              checked={role === 'subscriber'}
-              onChange={(e) => setRole(e.target.value)}
-            />
-            Subscriber
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="role"
-              value="moderator"
-              checked={role === 'moderator'}
-              onChange={(e) => setRole(e.target.value)}
-            />
-            Moderator
-          </label>
-        </div>
         <button className="join-btn" onClick={joinRoom}>
           Join Room
         </button>
